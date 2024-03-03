10ef655e30d34fb7751322b0d818b3e5b723fa74
Pixer9 <kyleboatright9@gmail.com>
2024-03-02 21:25:30 -0600

    Fix bug related to incorrect logging

--- logger.py
+++ logger.py
@@ -1,35 +1,24 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
-import datetime
+from utility import config
 import os
 
-class CustomFormatter(logging.Formatter):
-    def format(self, record):
-        if record.levelno == logging.ERROR or record.levelno == logging.CRITICAL:
-            record.message = f"{record.module} - {record.msg}"
-        else:
-            self._style = logging.PercentStyle("%(asctime)s - %(name)s - %(levelname)s - %(module)s - %(message)s")
-        return super().format(record)
-
-logs_dir = "logs"
+# Ensure log directory exists - this is where all written logs will go
+logs_dir = "logs" 
 if not os.path.exists(logs_dir):
     os.makedirs(logs_dir)
+
+log_filename = os.path.join(logs_dir, config.BOT_NAME + "-logs.txt")
+
+# Set up logger
+logger = logging.getLogger(__name__)
+
+stream_handler = logging.StreamHandler()
+
+file_handler = TimedRotatingFileHandler(filename=log_filename, when="midnight", backupCount=20)
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(module)s - %(message)s",
+    handlers=[stream_handler, file_handler] 
 )
-
-logger = logging.getLogger(__name__)
-
-custom_formatter = CustomFormatter()
-
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(custom_formatter)
-logger.addHandler(stream_handler)
-
-# Add TimedRotatingFileHandler to auto-rotate log files
-log_filename = os.path.join(logs_dir, datetime.datetime.now().strftime('%m-%d-%y') + ".txt")
-file_handler = TimedRotatingFileHandler(filename=log_filename, when="midnight", backupCount=20)
-file_handler.setFormatter(custom_formatter)
-logger.addHandler(file_handler)
