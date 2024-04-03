5728f051d000c055ba85dbaa483087966bade51d
Pixer9 <kyleboatright9@gmail.com>
2024-04-02 22:13:59 -0500

    Continue debug rotation

--- logger.py
+++ logger.py
@@ -3,12 +3,14 @@ from datetime import datetime
 #from utility import config
 from config import LoggerConfig as lc
 import logging
+import time
 import os
 
 # Ensure log directory exists - this is where all written logs will go
+
 if not os.path.exists(lc.LOGS_DIR):
     os.makedirs(lc.LOGS_DIR)
-
+'''
 class CustomTimedRotatingFileHandler(TimedRotatingFileHandler):
     
     def __init__(self, 
@@ -18,7 +20,7 @@ class CustomTimedRotatingFileHandler(TimedRotatingFileHandler):
                  encoding: str=lc.ENCODING, 
                  delay: bool=lc.DELAY,
                  utc: bool=lc.USE_UTC,
-                 atTime: str=lc.AT_TIME) -> None:
+                 atTime: datetime=lc.AT_TIME) -> None:
         self.dir_name = dir_name
         self.when = when
         self.backupCount = backupCount
@@ -27,6 +29,7 @@ class CustomTimedRotatingFileHandler(TimedRotatingFileHandler):
         self.encoding = encoding
         self.delay = delay
         file_name = self.get_new_log_file()
+
         super().__init__(filename=file_name, when=when, backupCount=backupCount, encoding=encoding, delay=delay, utc=utc, atTime=atTime)
 
 
@@ -41,10 +44,34 @@ class CustomTimedRotatingFileHandler(TimedRotatingFileHandler):
             Overridden method to change the way the new log file is named and
             avoid appending the date as a suffix.
         """
-        self.stream.close()
+        currentTime = int(time.time())
+        t = self.rolloverAt - self.interval
+
+        if self.utc:
+            timeTuple = time.gmtime(t)
+        else:
+            timeTuple = time.localtime(t)
+            dstNow = time.localtime(currentTime)[-1]
+            dstThen = timeTuple[-1]
+
+            if dstNow != dstThen:
+                if dstNow:
+                    addend = 3600
+                else:
+                    addend = -3600
+                timeTuple = time.localtime(t + addend)
+        
+        if self.stream:
+            self.stream.close()
+            self.stream = None
+
         self.baseFileName = self.get_new_log_file()
+        self.rotate(self.baseFileName, '')
+
         if not self.delay:
             self.stream = self._open()
+
+        self.rolloverAt = self.computeRollover(currentTime)
 
 
 log_filename = os.path.join(lc.LOGS_DIR, f"{lc.LOG_PREFIX}-log-{datetime.now().strftime('%Y-%m-%d')}.txt")
@@ -54,6 +81,39 @@ logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler()
 
 file_handler = CustomTimedRotatingFileHandler()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(module)s - %(message)s",
+    handlers=[stream_handler, file_handler]
+)
+'''
+
+class MidnightRotatingFileHandler(TimedRotatingFileHandler):
+    def __init__(self, filename, when=lc.WHEN, interval=1, backupCount=lc.BACKUP_COUNT, encoding=lc.ENCODING, delay=lc.DELAY, utc=lc.USE_UTC) -> None:
+        super().__init__(filename, when, interval, backupCount, encoding, delay, utc)
+        self.suffix_time = datetime.now().strftime("%Y-%m-%d")
+
+    def shouldRollover(self, record):
+        now = datetime.now()
+        if now.strftime("%Y-%m-%d") != self.suffix_time:
+            return 1
+        return 0
+    
+    def doRollover(self):
+        if self.stream:
+            self.stream.close()
+            self.stream = None
+        self.baseFileName = self.baseFileName.replace(self.suffix_time, datetime.now().strftime("%Y-%m-%d"))
+        self.mode = 'a'
+        self.stream = self._open()
+
+logger = logging.getLogger(__name__)
+log_filename = os.path.join(lc.LOGS_DIR, f"{lc.LOG_PREFIX}-log.log")
+
+stream_handler = logging.StreamHandler()
+
+file_handler = TimedRotatingFileHandler(filename=log_filename, when=lc.WHEN, interval=1, backupCount=lc.BACKUP_COUNT, encoding=lc.ENCODING, delay=lc.DELAY, utc=lc.USE_UTC, atTime=None)
 
 logging.basicConfig(
     level=logging.INFO,
